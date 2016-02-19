04211f904d2cec1bf04e6b9b698ad66ebe62c19d
M4nuski <the_man994@hotmail.com>
2016-02-18 21:33:31 -0500

    code commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace FolderCompare
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.logListBox = new System.Windows.Forms.ListBox();
+            this.baseFoldertextBox = new System.Windows.Forms.TextBox();
+            this.compareFoldersTextBox = new System.Windows.Forms.TextBox();
+            this.statusTextBox = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.strictcheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(221, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Base Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(245, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(221, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Add Folder to compare";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(935, 712);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(221, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Compare Folders and delete Duplicates";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // folderDialog1
+            // 
+            this.folderDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(473, 13);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(221, 39);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Clear Compare List";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(701, 13);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(221, 39);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Find Corrupted JPG";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(928, 13);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(221, 39);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Delete Empty Files/Folders";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // logListBox
+            // 
+            this.logListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logListBox.FormattingEnabled = true;
+            this.logListBox.ItemHeight = 17;
+            this.logListBox.Location = new System.Drawing.Point(16, 290);
+            this.logListBox.Name = "logListBox";
+            this.logListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.logListBox.Size = new System.Drawing.Size(1136, 395);
+            this.logListBox.TabIndex = 7;
+            // 
+            // baseFoldertextBox
+            // 
+            this.baseFoldertextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.baseFoldertextBox.Location = new System.Drawing.Point(16, 68);
+            this.baseFoldertextBox.Name = "baseFoldertextBox";
+            this.baseFoldertextBox.ReadOnly = true;
+            this.baseFoldertextBox.Size = new System.Drawing.Size(1133, 24);
+            this.baseFoldertextBox.TabIndex = 8;
+            // 
+            // compareFoldersTextBox
+            // 
+            this.compareFoldersTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.compareFoldersTextBox.Location = new System.Drawing.Point(16, 111);
+            this.compareFoldersTextBox.Multiline = true;
+            this.compareFoldersTextBox.Name = "compareFoldersTextBox";
+            this.compareFoldersTextBox.ReadOnly = true;
+            this.compareFoldersTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.compareFoldersTextBox.Size = new System.Drawing.Size(1133, 161);
+            this.compareFoldersTextBox.TabIndex = 9;
+            // 
+            // statusTextBox
+            // 
+            this.statusTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusTextBox.Location = new System.Drawing.Point(16, 712);
+            this.statusTextBox.Multiline = true;
+            this.statusTextBox.Name = "statusTextBox";
+            this.statusTextBox.ReadOnly = true;
+            this.statusTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.statusTextBox.Size = new System.Drawing.Size(902, 97);
+            this.statusTextBox.TabIndex = 10;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(935, 770);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(220, 39);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // strictcheckBox
+            // 
+            this.strictcheckBox.AutoSize = true;
+            this.strictcheckBox.Location = new System.Drawing.Point(932, 691);
+            this.strictcheckBox.Name = "strictcheckBox";
+            this.strictcheckBox.Size = new System.Drawing.Size(228, 22);
+            this.strictcheckBox.TabIndex = 12;
+            this.strictcheckBox.Text = "Strict (same name, size, hash)";
+            this.strictcheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 824);
+            this.Controls.Add(this.strictcheckBox);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.statusTextBox);
+            this.Controls.Add(this.compareFoldersTextBox);
+            this.Controls.Add(this.baseFoldertextBox);
+            this.Controls.Add(this.logListBox);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(1186, 274);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Compare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox logListBox;
+        private System.Windows.Forms.TextBox baseFoldertextBox;
+        private System.Windows.Forms.TextBox compareFoldersTextBox;
+        private System.Windows.Forms.TextBox statusTextBox;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox strictcheckBox;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace FolderCompare
+{
+    public partial class MainForm : Form
+    {
+        public struct FileData
+        {
+            public string fileName; // file.ext
+            public string filePath; // subpath/otherpath/file.ext
+            public byte[] hash;
+            public bool hashPerformed;
+            public long size;
+        }
+
+        private List<List<FileData>> compareFileLists;
+        private List<FileData> baseList;
+        private string basePath;
+        private int numDeleted;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private static string removePath(string FullName, string Path)
+        {
+            return FullName.Substring(Path.Length);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderDialog1.ShowDialog() == DialogResult.OK)
+            {
+                basePath = folderDialog1.SelectedPath;
+                baseFoldertextBox.Text = basePath;
+                logListBox.Items.Clear();
+                compareFoldersTextBox.Clear();
+                progressBar1.Value = 0;
+
+                //baseList = new List<FileData>();
+                baseList = listFiles(basePath, basePath);
+                compareFileLists = new List<List<FileData>>();
+
+                statusTextBox.Text = baseList.Count + " file(s) found in " + folderDialog1.SelectedPath;
+                button2.Enabled = true;
+            }
+        }
+        private static List<FileData> listFiles(string StartFolder, string CurrentFolder)
+        {
+            var DI = new DirectoryInfo(CurrentFolder);
+            var list = DI.GetFiles().Select(file => new FileData
+            {
+                fileName = file.Name,
+                filePath = removePath(file.FullName, StartFolder),
+                hashPerformed = false,
+                size = file.Length
+            }).ToList();
+            foreach (var folder in DI.GetDirectories())
+            {
+                list.AddRange(listFiles(StartFolder, folder.FullName));
+            }
+
+            return list;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if ((folderDialog1.ShowDialog() == DialogResult.OK) & (folderDialog1.SelectedPath != basePath))
+            {
+                compareFileLists.Add(listFiles(folderDialog1.SelectedPath, folderDialog1.SelectedPath));
+
+                compareFoldersTextBox.AppendText(folderDialog1.SelectedPath + "\r\n");
+
+                statusTextBox.AppendText("\r\n" + compareFileLists[compareFileLists.Count - 1].Count + " file(s) found in " + folderDialog1.SelectedPath);
+                button3.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            compareFoldersTextBox.Clear();
+            progressBar1.Value = 0;
+            compareFileLists = new List<List<FileData>>();
+            button3.Enabled = false;
+        }
+
+        static private bool compareHashData(byte[] a, byte[] b)
+        {
+            var result = (a.Length == b.Length);
+            if (result)
+            {
+                for (var i = 0; i < a.Length; i++)
+                {
+                    result = result & (a[i] == b[i]);
+                }
+            }
+            return result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            numDeleted = 0;
+            progressBar1.Maximum = compareFileLists.Count;
+
+            for (var i = 0; i < compareFileLists.Count; i++)
+            {
+                compareAndDelete(compareFileLists[i], compareFoldersTextBox.Lines[i]);
+                progressBar1.Value = i + 1;
+                Refresh();
+            }
+
+            statusTextBox.AppendText("\r\n" + numDeleted + " file(s) deleted");
+            button3.Enabled = false;
+        }
+
+        private void safeDelete(string fileName)
+        {
+            try
+            {
+                numDeleted++;
+                File.Delete(fileName);
+                logListBox.Items.Add(fileName + " deleted");
+            }
+            catch (IOException ex)
+            {
+                numDeleted--;
+                statusTextBox.AppendText("\r\n" + ex.Message);
+            }
+        }
+
+        private void compareAndDelete(List<FileData> listToCompare, string StartFolder)
+        {
+            for (var baseIndex = 0; baseIndex < baseList.Count; baseIndex++)
+            {
+                for (var compareIndex = 0; compareIndex < listToCompare.Count; compareIndex++)
+                {
+                    if ((baseList[baseIndex].size == listToCompare[compareIndex].size) && ((listToCompare[compareIndex].fileName == baseList[baseIndex].fileName) || !strictcheckBox.Checked))// Same Size
+                    {
+                        if (File.Exists(StartFolder + listToCompare[compareIndex].filePath))
+                        {
+                            var compareHashMaker = MD5.Create();
+                            var compareFileStream =
+                                File.OpenRead(StartFolder + listToCompare[compareIndex].filePath);
+                            var compareHashResult = compareHashMaker.ComputeHash(compareFileStream);
+                            compareHashMaker.Dispose();
+                            compareFileStream.Dispose();
+
+                            byte[] baseHashResult;
+
+                            if (baseList[baseIndex].hashPerformed) baseHashResult = baseList[baseIndex].hash;
+
+                            else
+                            {
+                                var baseHashMaker = MD5.Create();
+                                var baseFileStream = File.OpenRead(basePath + baseList[baseIndex].filePath);
+                                baseHashResult = baseHashMaker.ComputeHash(baseFileStream);
+                                baseHashMaker.Dispose();
+                                baseFileStream.Dispose();
+
+                                baseList[baseIndex] = new FileData
+                                {
+                                    fileName = baseList[baseIndex].fileName,
+                                    filePath = baseList[baseIndex].filePath,
+                                    hash = baseHashResult,
+                                    hashPerformed = true,
+                                    size = baseList[baseIndex].size
+                                };
+                            }
+                            // Same Data
+                            if (compareHashData(baseHashResult, compareHashResult))
+                                safeDelete(StartFolder + listToCompare[compareIndex].filePath);
+                        }
+                        else
+                        {
+                            statusTextBox.AppendText("\r\nFile not found (deleted?): " +
+                                                     listToCompare[compareIndex].filePath);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //delete empty files
+            logListBox.Items.Clear();
+            numDeleted = 0;
+            if (baseFoldertextBox.Text != "") searchEmptyFiles(baseFoldertextBox.Text);
+            if (compareFoldersTextBox.Lines.Any())
+            {
+                foreach (var paths in compareFoldersTextBox.Lines)
+                {
+                    if (paths != "") searchEmptyFiles(paths);
+                }
+            }
+            statusTextBox.AppendText("\r\n" + numDeleted + " empty file(s) deleted");
+
+            //delete empty folders
+            numDeleted = 0;
+            if (baseFoldertextBox.Text != "") searchEmptyFolders(baseFoldertextBox.Text);
+            if (compareFoldersTextBox.Lines.Any())
+            {
+                foreach (var paths in compareFoldersTextBox.Lines)
+                {
+                    if (paths != "") searchEmptyFolders(paths);
+                }
+            }
+            statusTextBox.AppendText("\r\n" + numDeleted + " empty folder(s) deleted");
+        }
+
+        private void searchEmptyFiles(string path)
+        {
+            var DI = new DirectoryInfo(path);
+            foreach (var file in DI.GetFiles())
+            {
+                if (file.Length == 0)
+                {
+                    logListBox.Items.Add(file.FullName);
+                    try
+                    {
+                        numDeleted++;
+                        File.Delete(file.FullName);
+                    }
+                    catch (Exception ex)
+                    {
+                        numDeleted--;
+                        statusTextBox.AppendText("\r\n" + ex.Message);
+                    }
+                }
+            }
+            foreach (var folder in DI.GetDirectories())
+            {
+                searchEmptyFiles(folder.FullName);
+            }
+        }
+
+        private void searchEmptyFolders(string path)
+        {
+            var DI = new DirectoryInfo(path);
+            foreach (var folder in DI.GetDirectories())
+            {
+                if (!folder.FullName.Contains(".git"))
+                {
+                    if ((!folder.GetFiles().Any()) & (!folder.GetDirectories().Any()))
+                    {
+                        logListBox.Items.Add(folder.FullName);
+                        try
+                        {
+                            numDeleted++;
+                            Directory.Delete(folder.FullName, false);
+                        }
+                        catch (Exception ex)
+                        {
+                            numDeleted--;
+                            statusTextBox.AppendText("\r\n" + ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        searchEmptyFolders(folder.FullName);
+                    }
+                }
+            }
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //find corrupted JPG
+            logListBox.Items.Clear();
+            numDeleted = 0;
+
+            if (baseFoldertextBox.Text != "") searchBadJPG(baseFoldertextBox.Text);
+            if (compareFoldersTextBox.Lines.Any())
+            {
+                foreach (var paths in compareFoldersTextBox.Lines)
+                {
+                    if (paths != "") searchBadJPG(paths);
+                }
+            }
+            statusTextBox.AppendText("\r\n" + numDeleted + " corrupted JPG(s) found");
+        }
+
+
+        private void searchBadJPG(string path)
+        {
+            var DI = new DirectoryInfo(path);
+            foreach (var file in DI.GetFiles())
+            {
+                if (string.Equals(file.Extension, ".jpg", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (!isValidJPG(file.FullName))
+                    {
+                        logListBox.Items.Add(file.FullName);
+                        numDeleted++;
+                    }
+                }
+            }
+            foreach (var folder in DI.GetDirectories())
+            {
+                searchBadJPG(folder.FullName);
+            }
+        }
+        private static bool isValidJPG(string file)
+        {
+            try
+            {
+                using (var buffer = Image.FromFile(file))
+                {
+                    return buffer.RawFormat.Equals(ImageFormat.Jpeg);
+                }
+            }
+            catch (OutOfMemoryException)
+            {
+                return false;
+            }
+        }
+
+
+
+
+    }
+}
